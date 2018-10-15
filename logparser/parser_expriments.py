e923cd260897dc3faa4ccf96c532504e81941c39
克里斯汀娜 <1206802260@qq.com>
2018-10-15 17:03:16 +0800

    Update parser_expriments.py

--- logparser/parser_expriments.py
+++ logparser/parser_expriments.py
@@ -10,6 +10,8 @@ import multiprocessing
 from logparser.parser import Spell, Drain
 from logparser.utils import visualize_spell_gvfile, visualize_drain_gvfile
 import gc
+import pandas as pd
+
 
 # ================ Ex1. 用于分析和比较各个parser的效果 ===================
 # generally idea:
@@ -19,23 +21,38 @@ import gc
 #   2. drain 能识别异常日志模式（突增的日志长度）， 但是drain算法被noise影响的问题比较严重 （有大量重复的日志signature 在不同的bucket上）
 
 # ---------------- step 1 : construct a parser with domain knowledge ---------------
-spell_parser = Spell(reg_file='./config/config.hdfs.txt', threshold=0.5)
-drain_parser = Drain(reg_file='./config/config.hdfs.txt', max_child=10, max_depth=4, min_similarity=0.5)
+# spell_parser = Spell(reg_file='./config/config.hdfs.txt', threshold=0.5)
+# drain_parser = Drain(reg_file='./config/config.hdfs.txt', max_child=10, max_depth=4, min_similarity=0.5)
+
+
+spell_parser = Spell(reg_file='./config/config.nova.txt', threshold=0.5)
+drain_parser = Drain(reg_file='./config/config.nova.txt', max_child=10, max_depth=4, min_similarity=0.5)
 
 
 # -------------------- step 2 : train data line step by line  -------------------
-with open('./data/hdfs_sample_2k.log', 'r') as f:
-    for line in f.readlines():
-        data = line.strip().split('\t')
-        log_id = int(data[0].strip())
-        log_entry = data[1]
-        # 输出的是每一次调用online_train所用的时间
-        spell_parser.online_train(log_entry, log_id)
-        drain_parser.online_train(log_entry, log_id)
+# with open('./data/hdfs_sample_2k.log', 'r') as f:
+#     for line in f.readlines():
+#         data = line.strip().split('\t')
+#         log_id = int(data[0].strip())
+#         log_entry = data[1]
+#         # 输出的是每一次调用online_train所用的时间
+#         spell_parser.online_train(log_entry, log_id)
+#         drain_parser.online_train(log_entry, log_id)
+
+
+# ----------------- 适用于dataframe的部分 ----------------
+# file = './data/zte_tongbu_filtered.csv'
+file = './data/zte_delete_filtered.csv'
+
+df = pd.read_csv(file)
+for idx, row in df.iterrows():
+    spell_parser.online_train(row.message, row.log_id)
+    drain_parser.online_train(row.message, row.log_id)
+
 
 # ----------------------------- result debug ------------------------------------
-for item in drain_parser.LogClusterMap:
-    print(item.cluster_id, item.log_template, item.log_ids)
+# for item in drain_parser.LogClusterMap:
+#     print(item.cluster_id, item.log_template, item.log_ids)
 
 gc.collect()
 
